76c32826837d9669d36e363a783da20f556a9354
Noskov Artem <nowasmawesome@gmail.com>
2020-07-20 13:54:36 +0500

    Updates

---
+++ precache-manifest.81922dbaf4e6a93dfaeeeb82fc574f17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae2ef14d9f25531aac91c0cd323900b7",
+    "url": "https://noartem.github.io/pomidor/index.html"
+  },
+  {
+    "revision": "f0ca3a873af7d4518a1c",
+    "url": "https://noartem.github.io/pomidor/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "cf4b701e8b8f302cba67",
+    "url": "https://noartem.github.io/pomidor/static/css/main.50c2fd6b.chunk.css"
+  },
+  {
+    "revision": "f0ca3a873af7d4518a1c",
+    "url": "https://noartem.github.io/pomidor/static/js/2.38053f2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://noartem.github.io/pomidor/static/js/2.38053f2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf4b701e8b8f302cba67",
+    "url": "https://noartem.github.io/pomidor/static/js/main.a20c2730.chunk.js"
+  },
+  {
+    "revision": "5c61298c986ea808843d",
+    "url": "https://noartem.github.io/pomidor/static/js/runtime-main.35da0b5c.js"
+  }
+]);
